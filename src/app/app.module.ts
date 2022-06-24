ee2b6d692be74a66b0055a75170514abd19eac3e
François HAUTEFEUILLE <francois.hautefeuille@hei.yncrea.fr>
2022-06-24 17:26:10 +0200

    welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { WelcomeModule } from './pages/welcome/welcome.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    WelcomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
